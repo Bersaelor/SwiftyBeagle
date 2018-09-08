5526594497e05adac8e3cadac12b97850d164ddc
Konrad Feiler <K_W_F@web.de>
2018-09-08 18:46:46 +0200

    make sure app runs on linux/docker

--- debugExecuteLocally.sh
+++ debugExecuteLocally.sh
@@ -1,3 +1,4 @@
 docker stop $(docker ps -aqf "ancestor=swiftybeagle") &> /dev/null && docker rm $(docker ps -aqf "ancestor=swiftybeagle") &> /dev/null
 docker build -t swiftybeagle . &&
-docker run -d -p 8080:8080 swiftybeagle
+docker run -d -p 8080:8080 swiftybeagle 
+docker attach $(docker ps -aqf "ancestor=swiftybeagle")
